3e6f4e7ca074b4a4b1473e8d59c60c2f5edcdacd
Larry Darryl Lee Jr <llee454@gmail.com>
2024-07-02 19:42:28 -0400

    Removed the init.scm file which wasnt used.

--- lib/init.scm
+++
@@ -1,9 +0,0 @@
-#|
-  Accepts one argument: n, an integer; and returns a string that
-  represents it.
-
-  Note: this function will accept a floating point number as well.
-  When passed a real number, it will round the number to its nearest
-  integer, and return that integer as a string.
-|#
-(define (int->string n) (num->string n 0))
